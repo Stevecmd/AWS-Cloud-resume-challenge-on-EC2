e2a3c5c2af4932941cae46a1a03a7d64e711a965
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-07-21 00:55:45 +0300

    Added an incremental counter

--- js/script.js
+++ js/script.js
@@ -48,21 +48,95 @@
 
 // updateCounter();
 
-// JS code version 3
-async function updateCounter() {
-    try {
-      let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
-      let data = await response.json();
+// JS code version 3 - works!
+// async function updateCounter() {
+//     try {
+//       let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+//       let data = await response.json();
   
-      if (data && data['Visit_Count']) {
-        var visitCount = data['Visit_Count'];
-        document.getElementById('counter').textContent = visitCount;
-      } else {
-        console.error("Unknown error occurred.");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
+//       if (data && data['Visit_Count']) {
+//         var visitCount = data['Visit_Count'];
+//         document.getElementById('counter').textContent = visitCount;
+//       } else {
+//         console.error("Unknown error occurred.");
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   }
+  
+//   updateCounter();  
+
+
+// // MongoDB atlas connection string
+// mongodb+srv://Cloud-resume-challenge:<password>@cloud-resume-challenge.62zy35f.mongodb.net/?retryWrites=true&w=majority
+// // Mongodb atlas code sample
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://Cloud-resume-challenge:M191VLsBir5YvVWx@cloud-resume-challenge.62zy35f.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://Cloud-resume-challenge:M191VLsBir5YvVWx@cloud-resume-challenge.62zy35f.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Connect to MongoDB
+async function connectToDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB Atlas!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas:", error);
   }
-  
-  updateCounter();  
+}
+
+connectToDB();
+
+// Define a route to handle the button click and increment the counter
+let clickCount = 0;
+app.post('/increment', async (req, res) => {
+  try {
+    clickCount++;
+
+    // Save the click count to the MongoDB database
+    const db = client.db("Cloud-resume-challenge-Database");
+    const collection = db.collection("Cloud-resume-challenge-view-count");
+    await collection.updateOne({}, { $set: { count: clickCount } }, { upsert: true });
+
+    res.json({ count: clickCount });
+  } catch (error) {
+    console.error("Error incrementing the count:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
